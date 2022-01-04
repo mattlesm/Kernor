19fd08d09ab22cc5ce1193dafee4228ad83b884f
mattlesm <mathieu.lessard.0@gmail.com>
2022-01-04 14:49:21 -0500

    paragraphs styling

--- components/Divinity.vue
+++ components/Divinity.vue
@@ -1,25 +1,28 @@
 <template>
     <v-container>
-        <v-row :align="align"> 
-            <v-col cols="3  ">
+        <v-row :align="align" class="first-row"> 
+            <v-col cols="3">
                 <h3>{{ divinityList.name }}<span class="alignment">{{ divinityList.alignment }}</span></h3>
             </v-col>
+            <v-spacer/>
+            <v-col class="specs" cols="1">
+                <h5><span class="element">ÉLÉMENT</span></h5>
+                <h5><span class="symbole">SYMBOLE</span></h5>
+            </v-col>
+            <v-divider
+                inset
+                vertical
+                class="divider"
+            ></v-divider>
+            <v-col class="specs-text" cols="4">
+                <h5>{{ divinityList.element }}</h5>
+                <h5>{{ divinityList.symbol }}</h5>
+            </v-col>
         </v-row>
 
-        <v-row>
+        <v-row class="rows">
             <v-col>
                 <h4>{{ divinityList.surname }}</h4>
-            </v-col>
-        </v-row>
-
-         <v-row>
-             <v-col cols="6">
-                <h5><span class="element">ÉLÉMENT</span>{{ divinityList.element }}</h5>
-            </v-col>
-         </v-row>
-         <v-row>
-            <v-col cols="6">
-                <h5><span class="symbole">SYMBOLE</span>{{ divinityList.symbol }}</h5>
             </v-col>
         </v-row>
 
@@ -29,9 +32,12 @@
             </v-col>
         </v-row>
 
-         <v-row>
+         <v-row class="rows"
+            v-for="text of divinityList.description"
+            :key="text"
+         >
              <v-col cols="10">
-                <p>{{ divinityList.description }}</p>
+                <p>{{ text }}</p>
             </v-col>
         </v-row>
     </v-container>
@@ -40,31 +46,32 @@
 <style>
     h3 {
         font-size: 350%;
-        margin-top: 10%;
-        margin-left: 10%;
     }
     .alignment {
         font-size: 30%;
         margin-left:5%;
     }
     h4 {
-        margin-left: 3%;
         margin-top: -2%;
         font-style: italic;
     }
-    h5 {
-        margin-left: 12%;
-    }
-    .element {
+    .specs {
         color: #FCA311;
-        margin-right:8%;
+        font-weight: bold;
+        margin-top: 2%;
     }
-    .symbole {
-        color: #FCA311;
-        margin-right:8%;
+    .specs-text {
+        margin-top: 2%;
+    }
+    .first-row {
+        margin-top: 4%;
+        margin-left: 2%;
+    }
+    .rows {
+        margin-left: 2%;
     }
     p {
-        margin-left:3%;
+        text-indent: 3%;
     }
 </style>
 

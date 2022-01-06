5eb94112fe0be8fece100f7a238b3d484c606b1f
mattlesm <mathieu.lessard.0@gmail.com>
2022-01-05 19:57:28 -0500

    maps upgrade

--- pages/maps.vue
+++ pages/maps.vue
@@ -9,38 +9,63 @@
             >
                 <v-hover>
                     <template v-slot:default="{ hover }">
-                            <v-img
-                                :src="item.path"
-                                aspect-ratio="1" 
-                            >
-                                <v-fade-transition>
-                                    <v-overlay
-                                        v-if="hover"
-                                        absolute
+                        <v-img
+                            :src="item.path"
+                            aspect-ratio="1" 
+                        >   
+                            <v-fade-transition>
+                                <v-overlay
+                                    v-if="hover"
+                                    absolute
+                                    dark
+                                >
+                                    <v-btn
                                         dark
+                                        plain
+                                        x-large
+                                        class="overlay_button"
+                                        @click="overlay = !overlay"
                                     >
-                                        <h6>
-                                            <v-btn
-                                                plain
-                                                @click="overlay = !overlay"
-                                            >
-                                                {{ item.title }}
-                                            </v-btn>
-                                        </h6>
-                                    </v-overlay>
-                                </v-fade-transition>
-                            </v-img>
+                                        {{ item.title }}
+                                    </v-btn>
+                                </v-overlay>
+                            </v-fade-transition>
+                        </v-img>
                     </template>
                 </v-hover>
             </v-col>
         </v-row>
         </div>
+        <v-overlay
+            :value="overlay"
+            class="overlay_wrapper"
+        >
+            <v-img :src="path" max-height="800px" contain>
+                <v-btn
+                    fab
+                    dark
+                    color="black"
+                    top
+                    small
+                    @click="overlay = false"
+                >
+                    <v-icon>mdi-close</v-icon>
+                </v-btn>
+            </v-img>
+        </v-overlay>
     </v-main>
 </template>
 
 <style>
     .wrapper {
         margin:2%;
+        height:100%;
+    }
+    .overlay_wrapper {
+        margin-left: 10%;
+    }
+    .overlay_button {
+        margin-top: 15%;
     }
 </style >
 
@@ -61,7 +86,9 @@
                 { id: 9, title: "Roc d'Or", path: require('@/assets/Roc_dOr.jpg')},
                 { id: 10, title: "Roc d'Or", path: require('@/assets/Roc_dOr.jpg')},
                 { id: 11, title: "Roc d'Or", path: require('@/assets/Roc_dOr.jpg')}
-            ]
+            ],
+            overlay: false,
+            path: require('@/assets/Kernor.jpg')
         }
     }
   };
